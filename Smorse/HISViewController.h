0569908cacd125de506e07d0467e298810bcd462
Git-Lucky <fraglerocked@gmail.com>
2014-01-20 11:34:27 -0800

    Initial Commit

---
+++ Smorse/HISViewController.h
@@ -0,0 +1,13 @@
+//
+//  HISViewController.h
+//  Smorse
+//
+//  Created by Tim Hise on 1/20/14.
+//  Copyright (c) 2014 GelRock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HISViewController : UIViewController
+
+@end
---
+++ Smorse/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Smorse
+//
+//  Created by Tim Hise on 1/20/14.
+//  Copyright (c) 2014 GelRock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HISAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HISAppDelegate class]));
+    }
+}
